baf886ad30a8f0366da2a8adc0ea60dc74b13782
Fred Sauer <fredsa@google.com>
2015-09-24 12:36:28 -0700

    ethan

---
+++ Assets/EnableMeshOnTriggerEnterController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnableMeshOnTriggerEnterController : MonoBehaviour
+{
+
+	MeshRenderer mr;
+
+	void Start ()
+	{
+		mr = GetComponent<MeshRenderer> ();
+		mr.enabled = false;
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		mr.enabled = true;
+	}
+	
+	void OnTriggerExit (Collider other)
+	{
+		mr.enabled = false;
+	}
+
+}
