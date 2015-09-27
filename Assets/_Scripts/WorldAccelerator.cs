2187e90542e21a16465222fc7263a380ad9c616d
Fred Sauer <fredsa@google.com>
2015-09-27 14:32:42 -0700

    toggle y / xyz axes

--- Assets/_Scripts/WorldAccelerator.cs
+++ Assets/_Scripts/WorldAccelerator.cs
@@ -6,10 +6,12 @@ public class WorldAccelerator : MonoBehaviour
 {
 	static float[] factors = {0f, .25f, .5f, .75f, 1f, 1.25f, 1.5f, 1.75f, 2f, 4f, 8f};
 
-	public Text text;
+	public Text valueText;
+	public Text axesText;
 
 	int factorIndex;
 	float factor;
+	bool xz;
 
 	void Start ()
 	{
@@ -28,22 +30,31 @@ public class WorldAccelerator : MonoBehaviour
 			UpdateFactor ();
 			Cardboard.SDK.Recenter ();
 		}
+		if (Cardboard.SDK.Tilted) {
+			xz = ! xz;
+			UpdateFactor ();
+		}
 		Vector3 euler = Cardboard.SDK.HeadPose.Orientation.eulerAngles;
 		euler.x = Normalize (euler.x);
 		euler.y = Normalize (euler.y);
 		euler.z = Normalize (euler.z);
 
-//		euler.x = transform.rotation.x;
-		euler.x *= factor;
+		if (xz) {
+			euler.x *= factor;
+			euler.z *= factor;
+		} else {
+			euler.x = transform.rotation.x;
+			euler.z = transform.rotation.z;
+		}
 		euler.y *= factor;
-		euler.z *= factor;
-//		euler.z = transform.rotation.z;
 		transform.rotation = Quaternion.Euler (euler);
 	}
 
 	void UpdateFactor ()
 	{
 		factor = factors [factorIndex];
-		text.text = string.Format ("+{0:0.00}", factor);
+		valueText.text = string.Format ("+{0:0.00}", factor);
+		axesText.text = xz ? "x/y/z-axes" : "y-axis";
+		axesText.color = xz ? Color.red : Color.white;
 	}
 }
