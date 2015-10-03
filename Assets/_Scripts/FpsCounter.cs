adb19e00b85799260c6501c64ff1f3f6ecc724da
Fred Sauer <fredsa@google.com>
2015-10-03 11:25:02 -0700

    red/green FPS counter

--- Assets/_Scripts/FpsCounter.cs
+++ Assets/_Scripts/FpsCounter.cs
@@ -4,8 +4,12 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Text))]
 public class FpsCounter : MonoBehaviour
 {
+	private static float BAD_FPS = 30f;
+	private static float GOOD_FPS = 60f;
+
 	private Text text;
 	private float fps = 60;
+	private Color color = new Color (0f, 0f, 0f);
 
 	void Awake ()
 	{
@@ -18,5 +22,12 @@ public class FpsCounter : MonoBehaviour
 		float currentFPS = 1.0f / Time.deltaTime;
 		fps = Mathf.Lerp (fps, currentFPS, interp);
 		text.text = string.Format ("{0:0} fps", fps);
+
+		// adjust red/green color
+		float fps_clamped = Mathf.Clamp (fps, BAD_FPS, GOOD_FPS);
+		float green_red_ratio = (fps_clamped - BAD_FPS) / (GOOD_FPS - BAD_FPS);
+		color.g = green_red_ratio;
+		color.r = 1f - green_red_ratio;
+		text.color = color;
 	}
 }
