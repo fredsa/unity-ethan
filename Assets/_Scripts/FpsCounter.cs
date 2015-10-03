e947bf0231c4d13f83806664ede177ff6feffe56
Fred Sauer <fredsa@google.com>
2015-10-03 11:10:42 -0700

    HUD FPS counter

---
+++ Assets/_Scripts/FpsCounter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class FpsCounter : MonoBehaviour
+{
+	private Text text;
+	private float fps = 60;
+
+	void Awake ()
+	{
+		text = GetComponent<Text> ();
+	}
+
+	void Update ()
+	{
+		float interp = Time.deltaTime / (0.5f + Time.deltaTime);
+		float currentFPS = 1.0f / Time.deltaTime;
+		fps = Mathf.Lerp (fps, currentFPS, interp);
+		text.text = string.Format ("{0:0} fps", fps);
+	}
+}
